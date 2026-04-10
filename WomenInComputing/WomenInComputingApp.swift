617f435a32a74121877b7c600227023a4eebd55f
Louise Verbeke <louiseverbeke@yahoo.be>
2026-04-09 22:13:08 -0400

    Initial Commit

---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Louise Verbeke on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
